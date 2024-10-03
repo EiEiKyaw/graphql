8d714157b359067211dbf5f59420329594f3a172
ei_ei_kyaw_97 <eieikyaw20101968@gmail.com>
2024-10-04 00:04:09 +0630

    add the sample query

--- apollo/index.js
+++ apollo/index.js
@@ -1,8 +1,85 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+import { typeDefs } from "./schema.js";
+import db from "./_db.js";
+
+const resolvers = {
+  // resolver
+  Query: {
+    games() {
+      return db.games;
+    },
+    // (parent, args, context)
+    // (_, args)
+    game(_, args) {
+      return db.games.find((data) => args.id === data.id);
+    },
+    authors() {
+      return db.authors;
+    },
+    author(_, args) {
+      return db.authors.find((data) => args.id === data.id);
+    },
+    reviews() {
+      return db.reviews;
+    },
+    review(_, args) {
+      return db.reviews.find((data) => args.id === data.id);
+    },
+  },
+  // resolver
+  Game: {
+    reviews(parent) {
+      return db.reviews.filter((data) => data.game_id === parent.id);
+    },
+  },
+  Author: {
+    reviews(parent) {
+      return db.reviews.filter((data) => data.game_id === parent.id);
+    },
+  },
+  Review: {
+    author(parent) {
+      return db.authors.find((data) => data.id == parent.author_id);
+    },
+    game(parent) {
+      return db.games.find((data) => data.id === parent.game_id);
+    },
+  },
+  Mutation: {
+    deleteGame(_, args) {
+      db.games = db.games.filter((data) => data.id !== args.id);
+      return db.games;
+    },
+    addGame(_, args) {
+      let game = {
+        ...args.game,
+        id: Math.floor(Math.random() * 10000).toString(),
+      };
+
+      db.games.push(game);
+      return game;
+    },
+    updateGame(_, args) {
+      db.games = db.games.map((data) => {
+        if (data.id === args.id) {
+          return { ...data, ...args.edits };
+        }
+        return data;
+      });
+      return db.games.find((data) => data.id === args.id);
+    },
+  },
+};
+
 // server setup
-const server = new ApolloServer({});
+const server = new ApolloServer({
+  // typeDefs >> definitions of types of data
+  typeDefs,
+  // resolvers >> function
+  resolvers,
+});
 
 const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
 
